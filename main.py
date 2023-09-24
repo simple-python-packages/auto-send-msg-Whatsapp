514485f6cb2c38c61cbd539d8109b9b32a726819
Azamat Dosmukhambetov <you@example.com>
2023-09-25 00:58:05 +0500

    fixed: updated

---
+++ main.py
@@ -0,0 +1,13 @@
+# pywhatkit kutibxonasini o'rnatib bo'lgandan keyin uni chaqirib olamiz
+import pywhatkit
+""" Oldin Googleda web.whatsapp.com da ro'yxatdan olgan bo'lishingiz kerak """
+
+# Example
+"""
+pywhatkit.sendwhatmsg("+998901234567", "Salom bu test xabar", 23,31)
+"""
+
+pywhatkit.sendwhatmsg("Raqam", "Xabar", "soat","daqiqa")
+#2 Raqam degan joyiga kimga xabar yubormoqchi bo'lsangiz o'shani raqamini yozasiz
+#3 Xabar degan joyiga kerakli xabarni yozasiz va xabar nechchida borishini yozasiz
+#4 loyihani ishga tushirasiz va biroz kutasiz, o'zi avtomatik  googledada whatsappga kirib ketadi va xabarni yuboradi
